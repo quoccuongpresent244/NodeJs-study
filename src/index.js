0d04f233959c2d6d1a7c0cbb6b4e1780415386fc
quoccuongpresent <nguyenduyanhquoc1st@gmail.com>
2021-10-12 08:41:58 +0700

    rm public/css

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, './public')))
+
 
 //HTTP logger
 app.use(morgan('combined'))
@@ -20,4 +21,4 @@ app.set('views', path.join(__dirname, 'resource/views'))
 app.get('/home', (req, res) => res.render('home'))
 app.get('/news', (req, res) => res.render('news'))
 
-app.listen(port, () => console.log('App listening at http://localhost:${port}'))
+app.listen(port, () => console.log('App listening at http://localhost:', port))
